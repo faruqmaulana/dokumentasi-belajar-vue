542b6ef209121c2ac2018a53f388e48a47d70a55
faruqmaulana <faruqchodot21@gmail.com>
2023-01-12 00:30:48 +0700

    chore: init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div>{{ count }}</div>
+  <button @click="addCount" :disabled="count >= 10">Add 1</button>
+  <button @click="reduceCount" :disabled="count <= 0">min 1</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return {
+      message: 'Hello World',
+      count: 0
+    }
+  },
+  methods:{
+    addCount(){
+      this.count++
+    } ,
+    reduceCount(){
+      this.count--
+    } 
+  }  
+
 }
 </script>
