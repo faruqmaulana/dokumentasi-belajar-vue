0d318358bb968a8b26cb0c1b157421377e3e4f26
faruqmaulana <faruqchodot21@gmail.com>
2023-01-12 01:11:25 +0700

    feat: v-model

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div>{{ message }}</div>
+
+  <input v-model="message"/>
+  
+  <!-- two way data binding -->
+  <input 
+    :value="message" 
+    @input="message = $event.target.value"
+  />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      message:'hi'
+    }
   }
 }
 </script>
