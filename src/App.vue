db2513bfe1a889e6ea6227670a9ba8102339d6b3
faruqmaulana <faruqchodot21@gmail.com>
2023-01-12 01:39:06 +0700

    feat: conditional rendering

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
 <template>
-  <div>{{ message }}</div>
-
-  <input v-model="message"/>
-  
-  <!-- two way data binding -->
-  <input 
-    :value="message" 
-    @input="message = $event.target.value"
-  />
+  <div v-if="status">Ini tampil</div>
+  <button @click="setStatus">toogle</button>
+  <div v-if="status">true</div>
+  <div v-else-if="status2">false</div>
+  <div v-else>arrrghh</div>
 </template>
 
 <script>
 
 export default {
-  data(){
-    return{
-      message:'hi'
+  data() {
+    return {
+      status: false,
+      status2: true
+    }
+  },
+  methods: {
+    setStatus(){
+      this.status = !this.status
     }
   }
 }
